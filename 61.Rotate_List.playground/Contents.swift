d0b37aabd0e60b1072aba7863282e6733c04ca19
EdwardBX <qq250799469@163.com>
2019-09-27 22:18:32 +0800

    day 4

---
+++ 61.Rotate_List.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+class Solution {
+    func rotateRight(_ head: ListNode?, _ k: Int) -> ListNode? {
+        guard head != nil else {
+            return head
+        }
+        
+        var p = head
+        var length = 1
+        while p?.next != nil {
+            p = p?.next
+            length += 1
+        }
+        let i = length - k % length
+        p?.next = head
+        
+        for _ in 0 ..< i {
+            p = p?.next
+        }
+        
+        let tmp = p?.next
+        p?.next = nil
+        
+        return tmp
+    }
+}
