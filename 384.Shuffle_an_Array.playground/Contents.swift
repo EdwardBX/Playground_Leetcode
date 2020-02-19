07991afa6448cdbcf7e37c9ed88cc75f80be7396
EdwardBX <qq250799469@163.com>
2020-02-19 12:07:50 +0800

    day0219

--- 384.Shuffle_an_Array.playground/Contents.swift
+++ 384.Shuffle_an_Array.playground/Contents.swift
@@ -28,10 +28,3 @@ class Solution {
         return Int.random(in: 0 ..< originalArray.count)
     }
 }
-
-/**
- * Your Solution object will be instantiated and called as such:
- * let obj = Solution(nums)
- * let ret_1: [Int] = obj.reset()
- * let ret_2: [Int] = obj.shuffle()
- */
