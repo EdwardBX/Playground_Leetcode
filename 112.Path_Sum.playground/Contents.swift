f9bf91853afc9e3ce70094319bda1536e870b9b4
EdwardBX <qq250799469@163.com>
2020-02-25 22:21:01 +0800

    day0225

---
+++ 112.Path_Sum.playground/Contents.swift
@@ -0,0 +1,22 @@
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+func hasPathSum(_ root: TreeNode?, _ sum: Int) -> Bool {
+    if (root?.val == sum && !((root?.left) != nil) && !((root?.right) != nil)) {
+        return true
+    }
+    if let value = root?.val {
+        let remain = sum - value
+        return hasPathSum(root?.left, remain) || hasPathSum(root?.right, remain)
+    } else {
+        return false
+    }
+}
